141ac26d0f05287a892152836b17666adcfcd9dd
Felix Dobslaw <felix.dobslaw@miun.se>
2022-08-03 09:28:53 +0200

    x

--- src/summarization/experimentsummary.jl
+++ src/summarization/experimentsummary.jl
@@ -1,7 +1,7 @@
 function summarize_per_sutname(expdir, sutname; wtd=false)
     summary_files = filter(f -> occursin(sutname * r"_\d*_all.csv", f) ,readdir(expdir)) # filenames
     summary_files |> println
-    all_dfs = map(f -> CSV.read(joinpath(expdir, f), DataFrame; type = String), summary_files)
+    all_dfs = map(f -> CSV.read(joinpath(expdir, f), DataFrame; types = String), summary_files)
     all_dfs = vcat(all_dfs...)
     all_dfs.count = parse.(Int64, all_dfs.count)
     gr = groupby(all_dfs, setdiff(names(all_dfs), ["count"]))
@@ -14,7 +14,7 @@ end
 
 function summarize_per_sutname_time(expdir, sutname, time; wtd=false)
     summary_files = filter(f -> startswith(f, "$(sutname)_") && endswith(f, "_$(time)_all.csv") ,readdir(expdir)) # filenames
-    all_dfs = map(f -> CSV.read(joinpath(expdir, f), DataFrame; type = String), summary_files)
+    all_dfs = map(f -> CSV.read(joinpath(expdir, f), DataFrame; types = String), summary_files)
     all_dfs = vcat(all_dfs...)
     all_dfs.count = parse.(Int64, all_dfs.count)
     gr = groupby(all_dfs, setdiff(names(all_dfs), ["count"]))
@@ -27,6 +27,7 @@ end
 
 function singlefilesummary(expdir::String; wtd=false)
     expfiles = filter(x -> isfile(x) && endswith(x, ".csv") && x != "results.csv" && !endswith(x, "_all.csv"), readdir(expdir))
+    expfiles |> println
     sutnames = unique(map(x -> split(x, "_")[1], expfiles))
     algs = unique(map(x -> split(x, "_")[3], expfiles))
     times = unique(map(x -> split(x, "_")[6], expfiles))
@@ -37,7 +38,7 @@ function singlefilesummary(expdir::String; wtd=false)
                 df = nothing
                 expfiles_sut = map(x -> "$(joinpath(expdir, x))", filter(x -> startswith(x, "$(sutname)_") && contains(x, "_$(time)_") && contains(x, "_$(alg)_") && !endswith(x, "_all.csv"), readdir(expdir)))
                 for expfile in expfiles_sut
-                    res_frame = CSV.read(expfile, DataFrame; type = String)
+                    res_frame = CSV.read(expfile, DataFrame; types = String)
                     res_frame.count = parse.(Int64, res_frame.count)
                     if isnothing(df)
                         df = res_frame              # init
