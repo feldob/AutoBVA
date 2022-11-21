e3bf07225b20123721daeee926e5b6d602c27ea1
Felix Dobslaw <felix.dobslaw@miun.se>
2022-11-21 14:36:25 +0100

    bounded number of entries in archive as option

--- src/detection/boundarycandidates.jl
+++ src/detection/boundarycandidates.jl
@@ -2,20 +2,29 @@
 # ------Boundary Candidate Tooling for storage and I/O----------
 #==============================================================#
 
+# TODO the default now is to set a limit at 20_000, that might be changed in future
 struct BoundaryCandidateArchive{T}
     sut::SUT{T}
     candidates::Dict{String, Integer}
+    max_entries::Integer
 
-    BoundaryCandidateArchive(sut::SUT{T}, candidates=Dict{String, Integer}()) where T = new{T}(sut, candidates)
+    function BoundaryCandidateArchive(sut::SUT{T}, candidates=Dict{String, Integer}(), max_entries = 20_000) where T
+        new{T}(sut, candidates, max_entries)
+    end
 end
 
 sut(bca::BoundaryCandidateArchive) = bca.sut
+max_entries(bca::BoundaryCandidateArchive) = bca.max_entries
 Base.size(bca::BoundaryCandidateArchive) = length(bca.candidates)
 
 exists(bca::BoundaryCandidateArchive, iₛ::AbstractString) = haskey(bca.candidates, iₛ)
 
 add_known(bca::BoundaryCandidateArchive, iₛ::AbstractString) = bca.candidates[iₛ] += 1
-add_new(bca::BoundaryCandidateArchive, iₛ::AbstractString) = bca.candidates[iₛ] = 1
+function add_new(bca::BoundaryCandidateArchive, iₛ::AbstractString)
+    if size(bca) < max_entries(bca)
+        bca.candidates[iₛ] = 1
+    end # ignore those that come in after. For smart search that learns this should be adapted to consider new entries and remove rate existing ones.
+end
 
 function add(bca::BoundaryCandidateArchive, iₛ::AbstractString)
     if exists(bca, iₛ)
